6f1297e07855fbbfee864a3f8b3f01ee06b96a79
Михаил <misga.yats@gmail.com>
2023-02-02 19:07:01 +0100

    trying to create right sample

---
+++ main_code/main.py
@@ -0,0 +1,7 @@
+from class_ import Tranzaction
+from sum_def import finding_ides, creating_classes_from_id, everything_for_class
+
+
+list_of_ides = finding_ides()
+test = everything_for_class(list_of_ides[0])
+print(test)

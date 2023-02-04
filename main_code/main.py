741a412abc9d41f795611a1b9b207ca9dbca0ddf
Михаил <misga.yats@gmail.com>
2023-02-04 15:14:39 +0100

    Основная ф-я уменьшена

--- main_code/main.py
+++ main_code/main.py
@@ -1,4 +1,4 @@
-from sum_def import finding_ides, getting_json_from_web
+from sum_def import finding_ides, getting_json_from_web, key_in_list
 
 file = getting_json_from_web()
 list_of_id = finding_ides()
@@ -9,19 +9,10 @@ for idefic in list_of_id:
             date_ = time_data[0].split('-')
             normal_date = f"{date_[2]}.{date_[1]}.{date_[0]}"
             disc = item['description']
-            if 'from' in item:
-                from_ = item['from'].split(' ')
-                if len(from_) == 3:
-                    from_num = from_[2]
-                    abs_from = f"{from_[0]} {from_[1]} {from_num[:4]} {from_num[4:6]}** **** {from_num[-4:]} -> "
-                else:
-                    from_num = from_[1]
-                    abs_from = f"{from_[0]} {from_num[:4]} {from_num[4:6]}** **** {from_num[-4:]} -> "
-            else:
-                abs_from = ''
+            abs_from = key_in_list('from', item)
             to = item['to'].split(' ')
             num_of_trans = to[1]
             summ = item["operationAmount"]["amount"]
             cur = item["operationAmount"]["currency"]["name"]
-            print(f"{normal_date} {disc}\n{abs_from}"
-                  f"{to[0]} **{num_of_trans[-4:]}\n{summ} {cur}\n")
+            print(f"""{normal_date} {disc}\n{abs_from}"""
+                  f"""{to[0]} **{num_of_trans[-4:]}\n{summ} {cur}\n""")

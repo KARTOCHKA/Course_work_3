2b9de189bcb8968977d575601442e80c3c62d0de
Михаил <misga.yats@gmail.com>
2023-02-04 15:52:35 +0100

    Написан первый тест

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
-import pytest
+from main_code.sum_def import getting_json_from_web
+
+test_link = "https://jsonkeeper.com/b/MBOG"
 
 
 def test_getting_json_from_web():
-    pass
+    assert getting_json_from_web(test_link) == [{"key":"test","number":1234,"sort_str":"string","long_str":"long_string"},{"key1":"test1","number1":1}]
